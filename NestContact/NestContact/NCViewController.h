d53207e89298f5a1da874b4d72db8e9f708a2f13
Yuan.Sun <sunyuan1713@gmail.com>
2013-09-16 01:22:22 +0800

    create project

---
+++ NestContact/NestContact/NCViewController.h
@@ -0,0 +1,13 @@
+//
+//  NCViewController.h
+//  NestContact
+//
+//  Created by sunny on 13-9-16.
+//  Copyright (c) 2013年 sunny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NCViewController : UIViewController
+
+@end
---
+++ NestContact/NestContact/NCViewController.m
@@ -0,0 +1,29 @@
+//
+//  NCViewController.m
+//  NestContact
+//
+//  Created by sunny on 13-9-16.
+//  Copyright (c) 2013年 sunny. All rights reserved.
+//
+
+#import "NCViewController.h"
+
+@interface NCViewController ()
+
+@end
+
+@implementation NCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
