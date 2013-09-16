88121d92d19c74ef9451f2ca943c3a1bae89a983
Yuan.Sun <sunyuan1713@gmail.com>
2013-09-16 12:24:47 +0800

    cocoa pods install

---
+++ NestContact/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// RHAddressBook
+#define COCOAPODS_POD_AVAILABLE_RHAddressBook
+#define COCOAPODS_VERSION_MAJOR_RHAddressBook 1
+#define COCOAPODS_VERSION_MINOR_RHAddressBook 0
+#define COCOAPODS_VERSION_PATCH_RHAddressBook 2
+
